42cf5a02d21d57e9b9f17d2e60e13f9755564d48
Joao Lucas <jlcarruda3@gmail.com>
2020-07-08 16:34:42 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Client } from 'boardgame.io/react'
+import { TicTacToe } from './Game'
+import { TicTacToeBoard } from './Board'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  debug: true
+})
 
 export default App;
